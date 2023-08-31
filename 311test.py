4a07367944f7fb7118c48653cfbcaa5341400985
bxharold <hfinzz@gmail.com>
2023-08-31 11:30:39 -0700

    yet another dry run.

---
+++ 311test.py
@@ -0,0 +1,11 @@
+# python 3.11 vs python 3.7 tester
+# used to confirm steps to create a github repo and to clone it to a pi
+
+match 1:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case _:
+        print("Something else")
+
